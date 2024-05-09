7fee381a50a7eb2799fdf98caa7249fe096e33dc
Sagaust <austinorge@yahoo.com>
2024-05-09 12:42:46 +0000

    my portfolio re uploaded to new repo

--- App.js
+++ App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Education from './components/Education/Education';
 import AddEducationModal from './components/Education/AddEducationModal';
 import Skill from './components/Skill/Skill';
-import { Switch } from 'react-router-dom'
 import AddSkillModal from './components/Skill/AddSkillModal';
 
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Route exact path="/" component={Education} />
-          <Route exact path="/add-education" component={AddEducationModal} />
-          <Route exact path="/skill" component={Skill} />
-          <Route exact path="/add-skill" component={AddSkillModal} />
-        </Switch>
+    <BrowserRouter>
+      <div> 
+          <Routes>
+            <Route path="/" element={<Education />} />
+            <Route path="/add-education" element={<AddEducationModal />} />
+            <Route path="/skill" element={<Skill />} />
+            <Route path="/add-skill" element={<AddSkillModal />} />
+          </Routes>
       </div>
-    </Router>
+    </BrowserRouter>
   );
 };
 
