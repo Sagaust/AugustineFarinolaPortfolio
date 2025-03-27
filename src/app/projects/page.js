7243e337c2db8daba45be0511965f88834081f09
Sagaust <austinorge@yahoo.com>
2025-03-27 06:03:04 +0000

    error fix

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,28 +1,27 @@
-import React, { Component } from 'react';
-import ProjectItem from '../components/ProjectItem';
-import projectData from '../projectData.json';
+// src/app/projects/page.js
+'use client';
 
-class Projects extends Component {
-render() {
-return (
-<div className="container">
-<h2>Projects</h2>
-<div className="row">
-{projectData.map(project => (
-<div className="col-md-6 my-3" key={project.id}>
-<ProjectItem
-             title={project.title}
-             description={project.description}
-             image={project.image}
-             demoLink={project.demoLink}
-             codeLink={project.codeLink}
-           />
-</div>
-))}
-</div>
-</div>
-);
-}
-}
+import React from 'react';
+import ProjectItem from '@/components/ProjectItem';
+import projectData from '@/projectData.json';
 
-export default Projects;
+export default function Projects() {
+  return (
+    <div className="container my-5">
+      <h2 className="mb-4">Projects</h2>
+      <div className="row">
+        {projectData.map(project => (
+          <div className="col-md-6 my-3" key={project.id}>
+            <ProjectItem
+              title={project.title}
+              description={project.description}
+              image={project.image}
+              demoLink={project.demoLink}
+              codeLink={project.codeLink}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,18 +1,39 @@
+// src/components/ProjectItem.js
+'use client';
+
 import React from 'react';
 import { Card, Button } from 'react-bootstrap';
+import styles from './ProjectItem.module.css';
 
-const ProjectItem = ({ id, title, description, image,demoLink, codeLink }) => {
+export default function ProjectItem({ title, description, image, demoLink, codeLink }) {
   return (
-    <Card>
-      <Card.Img variant="top" src={image} />
+    <Card className={styles.projectCard}>
+      <Card.Img variant="top" src={image} alt={title} />
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Text>{description}</Card.Text>
-        <Button variant="primary" href={demoLink}  target="_blank">Demo</Button>{' '}
-        <Button variant="secondary" href={codeLink}  target="_blank">Code</Button>
+        <div className={styles.buttonGroup}>
+          {demoLink && (
+            <Button 
+              variant="primary" 
+              href={demoLink} 
+              target="_blank"
+              className="me-2"
+            >
+              Live Demo
+            </Button>
+          )}
+          {codeLink && (
+            <Button 
+              variant="secondary" 
+              href={codeLink} 
+              target="_blank"
+            >
+              View Code
+            </Button>
+          )}
+        </div>
       </Card.Body>
     </Card>
   );
-};
-
-export default ProjectItem;
+}
